677087d1e9a7bb22fc25505cceba4af89f02234b
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-11-15 16:36:06 -0700

    cleanup

--- src/theme.js
+++ src/theme.js
@@ -6,16 +6,7 @@ const theme = createTheme(
   
 //Paste values from ThemeCustomizer themeObject property here
 
-{
-  palette: {
-    primary: {
-      main: '#556cd6',
-    },
-    secondary: {
-      main: '#19857b',
-    }
-  },
-}
+{ palette: { mode: 'light', primary: { main: '#68518f', light: '#9575cd', dark: '#1565c0', contrastText: '#f3f3f3' }, secondary: { main: '#b26e59', light: '#ff9e80', dark: '#7b1fa2', contrastText: '#ffffff' } }, typography: { fontSize: 14, fontFamily: 'Noto Sans, Helvetica, Arial, sans-serif' }, shape: { borderRadius: 8 }, components: { muiButton: { styleOverrides: { root: { fontSize: '' } } } } }
 
 //End theme
 );
