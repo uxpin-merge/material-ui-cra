fa8c9e2c64b8338f1b285fbf72673804b05566ee
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-11-28 12:00:38 -0700

    CLEANUP

--- src/theme.js
+++ src/theme.js
@@ -5,16 +5,45 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme(
   
 //Paste values from ThemeCustomizer themeObject property here
-
 {
-  palette: {
-    primary: {
-      main: '#556cd6',
-    },
-    secondary: {
-      main: '#19857b',
-    }
+  "palette": {
+      "mode": "light",
+      "primary": {
+          "main": "#1976d2",
+          "light": "#42a5f5",
+          "dark": "#1565c0",
+          "contrastText": "#ffffff"
+      },
+      "secondary": {
+          "main": "#9c27b0",
+          "light": "#ba68c8",
+          "dark": "#7b1fa2",
+          "contrastText": "#ffffff"
+      }
   },
+  "typography": {
+      "fontFamily": "Roboto, Helvetica, Arial, sans-serif"
+  },
+  "shape": {
+      "borderRadius": 4
+  },
+  "components": {
+      "MuiCssBaseline": {
+          "styleOverrides": {
+              "body": {
+                  "fontSize": 14,
+                  "color": "inherit"
+              }
+          }
+      },
+      "MuiButton": {
+          "styleOverrides": {
+              "root": {
+                  "fontSize": ""
+              }
+          }
+      }
+  }
 }
 
 //End theme
